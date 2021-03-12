7dc6034ff65c837c0ea81e9d8df0dee3d5c5c34c
AlhajiTaibu <alhabdutaib@gmail.com>
2021-03-12 16:21:41 +0000

    Initial commit

---
+++ src/main/java/io/turntabl/reportingservices/ReportingServicesApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.reportingservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/reportingservices/ReportingServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.reportingservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
